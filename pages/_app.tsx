9b602194f9b584da33e588735a204c72664b5019
hugojvb <hugojvb20@gmail.com>
2021-10-06 01:01:04 +0100

    Fixed custom document and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { FC } from "react";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../styles/theme";
 
-export default function MyApp(props: any) {
-	const { Component, pageProps } = props;
-
+export default function MyApp({
+	Component,
+	pageProps,
+}: {
+	Component: FC<typeof pageProps>;
+	pageProps: Object;
+}) {
 	React.useEffect(() => {
 		// Remove the server-side injected CSS.
 		const jssStyles = document.querySelector("#jss-server-side");
@@ -33,8 +36,3 @@ export default function MyApp(props: any) {
 		</React.Fragment>
 	);
 }
-
-MyApp.propTypes = {
-	Component: PropTypes.elementType.isRequired,
-	pageProps: PropTypes.object.isRequired,
-};
